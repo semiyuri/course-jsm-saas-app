4ae7729902196bc70cc4fc5c0808bb289bbf60a9
semiyuri <juriyunna27@gmail.com>
2025-06-22 00:58:33 +0300

    chore: init shadcn components library dependencies

---
+++ types/index.ts
@@ -0,0 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+enum Subject {
+  maths = "maths",
+  language = "language",
+  science = "science",
+  history = "history",
+  coding = "coding",
+  geography = "geography",
+  economics = "economics",
+  finance = "finance",
+  business = "business",
+}
