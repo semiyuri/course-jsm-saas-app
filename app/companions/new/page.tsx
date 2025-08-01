a9c86a8b8e13633a6f492ea9b28505242b66ac3a
semiyuri <juriyunna27@gmail.com>
2025-08-02 02:08:34 +0300

    feat: implement companions limit through clerk subscription feature gates

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,5 +1,8 @@
 import CompanionForm from "@/components/companion-form";
+import { newCompanionPermissions } from "@/lib/actions/companion.actions";
 import { auth } from "@clerk/nextjs/server";
+import Image from "next/image";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 import React from "react";
 
@@ -10,12 +13,37 @@ const Page = async () => {
     redirect("/sign-in");
   }
 
+  const canCreateCompanion = await newCompanionPermissions();
+
   return (
     <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
-      <article className="w-full gap-6 flex flex-col">
-        <h1>Companion Builder</h1>
-        <CompanionForm />
-      </article>
+      {canCreateCompanion ? (
+        <article className="w-full gap-6 flex flex-col">
+          <h1>Companion Builder</h1>
+          <CompanionForm />
+        </article>
+      ) : (
+        <article className="companion-limit">
+          <Image
+            src="/images/limit.svg"
+            alt="Companion Limit"
+            width={360}
+            height={230}
+          />
+
+          <div className="cta-badge">Upgrade your plan</div>
+
+          <h1>You&apos;ve reached your companion limit</h1>
+          <p>You need to upgrade your plan to create more companions.</p>
+
+          <Link
+            href="/subscription"
+            className="btn-primary w-full justify-center"
+          >
+            Upgrade My Plan
+          </Link>
+        </article>
+      )}
     </main>
   );
 };
--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -132,3 +132,29 @@ export const getUserCompanions = async (userId: string) => {
 
   return data;
 };
+
+export const newCompanionPermissions = async () => {
+  const { userId, has } = await auth();
+  const supabase = createSupabaseClient();
+
+  let limit = 0;
+
+  if (has({ plan: "pro" })) {
+    return true;
+  } else if (has({ feature: "3_active_companions" })) {
+    limit = 3;
+  } else if (has({ feature: "10_active_companions" })) {
+    limit = 10;
+  }
+
+  const { data, error } = await supabase
+    .from("companions")
+    .select("id", { count: "exact" })
+    .eq("author", userId);
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return data.length < limit;
+};
